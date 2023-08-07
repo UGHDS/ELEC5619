d28e44f181a73641f9c4046a15754c4b7a67a194
Shuaiqing Wang <swan0197@uni.sydney.edu.au>
2023-08-07 10:36:03 +1000

    Initial spring setup.

---
+++ src/main/java/com/usyd/group08/elec5619/Elec5619Application.java
@@ -0,0 +1,13 @@
+package com.usyd.group08.elec5619;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elec5619Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elec5619Application.class, args);
+	}
+
+}
